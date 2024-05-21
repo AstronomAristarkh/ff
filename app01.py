c113cba22f0892f8fa7fbb098f1570244ff69b00
astronom <chasovnikov.ar17@physics.msu.ru>
2024-05-21 13:01:06 +0300

    betavers

---
+++ app01.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/index/')
+def html_index():
+    return render_template('index.html')
+
+@app.route('/clothes/')
+def html_clothes():
+    return render_template('clothes.html')
+
+@app.route('/jacket/')
+def html_jacket():
+    return render_template('jacket.html')
+
+@app.route('/shoes/')
+def html_shoes():
+    return render_template('shoes.html')
+
+if __name__ == '__main__':
+    app.run()
---
+++ wsgi.py
@@ -0,0 +1,3 @@
+from app01 import app
+if __name__ == "__main__":
+    app.run(debug=True)
